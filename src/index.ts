8457bf9d0ad79d7b36e6e1fdc8b0c3f183e64b98
jdami <jiangjk1990@gmail.com>
2022-10-09 15:20:46 +0800

    修复在小程序环境下 没有sessionStorage报错问题
    Signed-off-by: 江计康 <jiangjikang@xwtec.cn>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PiniaPluginContext } from 'pinia'
+import {PiniaPluginContext} from 'pinia'
 
 export interface PersistStrategy {
   key?: string;
@@ -21,9 +21,11 @@ declare module 'pinia' {
 }
 
 export const updateStorage = (strategy: PersistStrategy, store: Store) => {
-  const storage = strategy.storage || sessionStorage
+  const storage = strategy.storage || window.sessionStorage
   const storeKey = strategy.key || store.$id
-
+  if (!storage) {
+    throw new Error('no storage!')
+  }
   if (strategy.paths) {
     const partialState = strategy.paths.reduce((finalObj, key) => {
       finalObj[key] = store.$state[key]
@@ -36,17 +38,20 @@ export const updateStorage = (strategy: PersistStrategy, store: Store) => {
   }
 }
 
-export default ({ options, store }: PiniaPluginContext): void => {
+export default ({options, store}: PiniaPluginContext): void => {
   if (options.persist?.enabled) {
     const defaultStrat: PersistStrategy[] = [{
       key: store.$id,
-      storage: sessionStorage,
+      storage: window.sessionStorage,
     }]
 
     const strategies = options.persist?.strategies?.length ? options.persist?.strategies : defaultStrat
 
     strategies.forEach((strategy) => {
-      const storage = strategy.storage || sessionStorage
+      const storage = strategy.storage || window.sessionStorage
+      if (!storage) {
+        throw new Error('no storage!')
+      }
       const storeKey = strategy.key || store.$id
       const storageResult = storage.getItem(storeKey)
 
